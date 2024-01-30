b28ff8171fab09c7157e7dbb8d7674be893e503b
Maria Luz <luzdudinha@gmail.com>
2024-01-30 17:02:40 -0300

    semaphore added

--- C-SID/src/com/zetcode/Board.java
+++ C-SID/src/com/zetcode/Board.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 import javax.swing.*;
 
 public class Board extends JPanel implements Runnable {
@@ -29,6 +31,8 @@ public class Board extends JPanel implements Runnable {
 
     private Timer timer;
     private Thread animator;
+
+    private Semaphore alienSemaphore;
 
     private BufferedImage bufferImage;
 
@@ -49,6 +53,7 @@ public class Board extends JPanel implements Runnable {
 
     private void gameInit() {
         aliens = new ArrayList<>();
+        alienSemaphore = new Semaphore(Commons.NUMBER_OF_ALIENS_TO_DESTROY);
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 6; j++) {
@@ -123,128 +128,144 @@ public class Board extends JPanel implements Runnable {
                 Commons.BOARD_WIDTH / 2);
     }
 
-    private void update() {
+    private void update(){    
         if (deaths == Commons.NUMBER_OF_ALIENS_TO_DESTROY) {
-            inGame = false;
-            timer.stop();
-            message = "Game won!";
+        inGame = false;
+        timer.stop();
+        message = "Game won!";
         }
 
-        // player
-        player.act();
+    // player
+    player.act();
 
-        // shot
-        if (shot.isVisible()) {
-            int shotX = shot.getX();
-            int shotY = shot.getY();
-
-            for (Alien alien : aliens) {
-                int alienX = alien.getX();
-                int alienY = alien.getY();
-
-                if (alien.isVisible() && shot.isVisible()) {
-                    if (shotX >= (alienX) && shotX <= (alienX + Commons.ALIEN_WIDTH) && shotY >= (alienY)
-                            && shotY <= (alienY + Commons.ALIEN_HEIGHT)) {
-
-                        var ii = new ImageIcon(explImg);
-                        alien.setImage(ii.getImage());
-                        alien.setDying(true);
-                        deaths++;
-                        shot.die();
-                    }
-                }
-            }
-
-            int y = shot.getY();
-            y -= 6;
-
-            if (y < 0) {
-                shot.die();
-            } else {
-                shot.setY(y);
-            }
-        }
-
-        // aliens
-        for (Alien alien : aliens) {
-            int x = alien.getX();
-
-            if (x >= Commons.BOARD_WIDTH - Commons.BORDER_RIGHT && direction != -1) {
-                direction = -1;
-
-                Iterator<Alien> i1 = aliens.iterator();
-                while (i1.hasNext()) {
-                    Alien a2 = i1.next();
-                    a2.setY(a2.getY() + Commons.GO_DOWN);
-                }
-            }
-
-            if (x <= Commons.BORDER_LEFT && direction != 1) {
-                direction = 1;
-
-                Iterator<Alien> i2 = aliens.iterator();
-                while (i2.hasNext()) {
-                    Alien a = i2.next();
-                    a.setY(a.getY() + Commons.GO_DOWN);
-                }
-            }
-        }
-
-        Iterator<Alien> it = aliens.iterator();
-
-        while (it.hasNext()) {
-            Alien alien = it.next();
-
-            if (alien.isVisible()) {
-                int y = alien.getY();
-
-                if (y > Commons.GROUND - Commons.ALIEN_HEIGHT) {
-                    inGame = false;
-                    message = "Invasion!";
-                }
-
-                alien.act(direction);
-            }
-        }
-
-        // bombs
-        var generator = new Random();
+    // shot
+    if (shot.isVisible()) {
+        int shotX = shot.getX();
+        int shotY = shot.getY();
 
         for (Alien alien : aliens) {
-            int shot = generator.nextInt(15);
-            Alien.Bomb bomb = alien.getBomb();
+            int alienX = alien.getX();
+            int alienY = alien.getY();
 
-            if (shot == Commons.CHANCE && alien.isVisible() && bomb.isDestroyed()) {
-                bomb.setDestroyed(false);
-                bomb.setX(alien.getX());
-                bomb.setY(alien.getY());
-            }
-
-            int bombX = bomb.getX();
-            int bombY = bomb.getY();
-            int playerX = player.getX();
-            int playerY = player.getY();
-
-            if (player.isVisible() && !bomb.isDestroyed()) {
-                if (bombX >= (playerX) && bombX <= (playerX + Commons.PLAYER_WIDTH) && bombY >= (playerY)
-                        && bombY <= (playerY + Commons.PLAYER_HEIGHT)) {
+            if (alien.isVisible() && shot.isVisible()) {
+                if (shotX >= (alienX) && shotX <= (alienX + Commons.ALIEN_WIDTH) && shotY >= (alienY)
+                        && shotY <= (alienY + Commons.ALIEN_HEIGHT)) {
 
                     var ii = new ImageIcon(explImg);
-                    player.setImage(ii.getImage());
-                    player.setDying(true);
-                    bomb.setDestroyed(true);
+                    alien.setImage(ii.getImage());
+                    alien.setDying(true);
+                    deaths++;
+                    shot.die();
+
+                    // acquire the semaphore before launching the alien's bomb
+                    try {
+                        alienSemaphore.acquire();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // launch the alien's bomb
+                    Alien.Bomb bomb = alien.getBomb();
+                    bomb.setDestroyed(false);
+                    bomb.setX(alien.getX());
+                    bomb.setY(alien.getY());
                 }
             }
+        }
 
-            if (!bomb.isDestroyed()) {
-                bomb.setY(bomb.getY() + 1);
+        int y = shot.getY();
+        y -= 6;
 
-                if (bomb.getY() >= Commons.GROUND - Commons.BOMB_HEIGHT) {
-                    bomb.setDestroyed(true);
-                }
+        if (y < 0) {
+            shot.die();
+        } else {
+            shot.setY(y);
+        }
+    }
+
+    // aliens
+    for (Alien alien : aliens) {
+        int x = alien.getX();
+
+        if (x >= Commons.BOARD_WIDTH - Commons.BORDER_RIGHT && direction != -1) {
+            direction = -1;
+
+            Iterator<Alien> i1 = aliens.iterator();
+            while (i1.hasNext()) {
+                Alien a2 = i1.next();
+                a2.setY(a2.getY() + Commons.GO_DOWN);
+            }
+        }
+
+        if (x <= Commons.BORDER_LEFT && direction != 1) {
+            direction = 1;
+
+            Iterator<Alien> i2 = aliens.iterator();
+            while (i2.hasNext()) {
+                Alien a = i2.next();
+                a.setY(a.getY() + Commons.GO_DOWN);
             }
         }
     }
+
+    Iterator<Alien> it = aliens.iterator();
+
+    while (it.hasNext()) {
+        Alien alien = it.next();
+
+        if (alien.isVisible()) {
+            int y = alien.getY();
+
+            if (y > Commons.GROUND - Commons.ALIEN_HEIGHT) {
+                inGame = false;
+                message = "Invasion!";
+            }
+
+            alien.act(direction);
+        }
+    }
+
+    // bombs
+    var generator = new Random();
+
+    for (Alien alien : aliens) {
+        int shot = generator.nextInt(15);
+        Alien.Bomb bomb = alien.getBomb();
+
+        if (shot == Commons.CHANCE && alien.isVisible() && bomb.isDestroyed()) {
+            bomb.setDestroyed(false);
+            bomb.setX(alien.getX());
+            bomb.setY(alien.getY());
+        }
+
+        int bombX = bomb.getX();
+        int bombY = bomb.getY();
+        int playerX = player.getX();
+        int playerY = player.getY();
+
+        if (player.isVisible() && !bomb.isDestroyed()) {
+            if (bombX >= (playerX) && bombX <= (playerX + Commons.PLAYER_WIDTH) && bombY >= (playerY)
+                    && bombY <= (playerY + Commons.PLAYER_HEIGHT)) {
+
+                var ii = new ImageIcon(explImg);
+                player.setImage(ii.getImage());
+                player.setDying(true);
+                bomb.setDestroyed(true);
+
+                // release the semaphore after the bomb has been launched
+                alienSemaphore.release();
+            }
+        }
+
+        if (!bomb.isDestroyed()) {
+            bomb.setY(bomb.getY() + 1);
+
+            if (bomb.getY() >= Commons.GROUND - Commons.BOMB_HEIGHT) {
+                bomb.setDestroyed(true);
+            }
+        }
+    }
+}
 
     private void doGameCycle() {
         update();
